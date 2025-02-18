1c1610d6a401b2c8aedb412a8935c1b4e0d268bb
Enfraktro <101732377+Enfraktro@users.noreply.github.com>
2025-02-18 21:51:58 +0200

    Add files via upload

---
+++ TicketManagmnet/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketManagmnet
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_signupBtn_Click(object sender, EventArgs e)
+        {
+            RegForm regForm = new RegForm();
+            regForm.Show();
+
+            this.Hide();
+        }
+    }
+}
